aa2d1bbb344da91ef27cfa04867e6066e440fc17
Josué M. Sehnem <josue@sehnem.com>
2017-11-21 08:51:37 -0200

    update

--- tests/satpy_goes.py
+++ tests/satpy_goes.py
@@ -5,13 +5,37 @@ from datetime import datetime
 from satpy.resample import get_area_def
 
 
-goes = Goes16('ABI-L1b-RadF', datetime(2017,11,20,15), bands=1)
+goes = Goes16('ABI-L1b-RadF', datetime(2017,11,20,15), bands=list(range(1,4)))
 goes.download('/home/josue/Documentos/DataAnalysis/goes/data/')
 
 scn = Scene(
-    base_dir="/home/josue/Documentos/DataAnalysis/goes/data/",
+    filenames=glob.glob("/home/josue/Documentos/DataAnalysis/goes/data/*"),
     reader='abi_l1b')
 
-bra = scn.resample("brazil2")
-bra.show('overview')
+for ch in ["C{channel:02d}".format(channel=chn + 1) for chn in range(3)]:
+    scn.load([ch])
 
+bra = scn.resample("southamerica")
+del scn
+
+
+
+from trollimage.image import Image
+import numpy as np
+
+ch1 = bra.datasets['C01']
+ch2 = bra.datasets['C02']
+ch3 = bra.datasets['C03']
+
+
+green = 0.48358168 * ch2 + 0.45603695 * ch1 + 0.06038137 * ch3
+
+img = Image((ch2, green, ch1), mode='RGB')
+
+
+img.stretch_hist_equalize(0)
+img.stretch_hist_equalize(1)
+img.stretch_hist_equalize(2)
+img.gamma()
+pi = img.pil_image()#.resize((768,768))
+pi.save('ant.png')
